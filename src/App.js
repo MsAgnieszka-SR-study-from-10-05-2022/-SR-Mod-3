fffbc43e31a2451b867389c033510b119c506190
Agnieszka Uzarek <agnieszka.czerebak@gmail.com>
2022-06-05 18:36:59 +0200

    Initialized project using create-react-app & cleared

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p>Here will be the body of our app.</p>
+    </div>
+  );
+};
+
+export default App;
